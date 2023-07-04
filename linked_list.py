3d21908c912cfee7a6914d5d039c5e8eba589870
Abhilashg11 <alexrog1122@gmail.com>
2023-07-04 23:03:24 +0530

    main

--- linked_list.py
+++ linked_list.py
@@ -6,21 +6,21 @@ node1 = node(10)
 print(node)
 
 
-#create class for linking
+create class for linking
 
-# class linkedlist:
-#     def __init__(self):
-#         self.head = None 
-#     def print_ll(self):
-#         if self.head is None:
-#             print("linked list is empty")
-#         else:
-#             n= self.head
-#             while n == None:
-#                 print(n.data)
-#                 n = n.ref
+class linkedlist:
+    def __init__(self):
+        self.head = None 
+    def print_ll(self):
+        if self.head is None:
+            print("linked list is empty")
+        else:
+            n= self.head
+            while n == None:
+                print(n.data)
+                n = n.ref
 
-# ll1 = linkedlist()
-# ll1.print_ll()
+ll1 = linkedlist()
+ll1.print_ll()
 
     
