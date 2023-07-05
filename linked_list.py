69df3cf6251072205b6dc037b8503795f088870e
Abhilashg11 <alexrog1122@gmail.com>
2023-07-05 22:21:35 +0530

    main

--- linked_list.py
+++ linked_list.py
@@ -11,17 +11,31 @@ class node:
 class linkedlist:
     def __init__(self):
         self.head = node()
-
+# adding node
     def append(self,data):
         new_node = node(data) 
         cur = self.head    
         while cur.ref != None:
            cur = cur.ref
         cur.ref = new_node
-        
+
+# finding length
     def length(self):
         cur = self.head
         total = 0 
+        while cur.ref!= None:
+            total+=1
+            cur = cur.ref
+        return total    
+
+    def display(self):
+        elems = []
+        cur_node - self.head
+        while  cur_node !=None:
+            cur_node = cur_node.ref
+            elems.append(cur_node.data)
+        print(elems)    
+
 
 
 
