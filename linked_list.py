cc86c41ed16a9de47a90e75af67ceedc3ce75f90
Abhilashg11 <alexrog1122@gmail.com>
2023-07-19 18:25:04 +0530

    main

--- linked_list.py
+++ linked_list.py
@@ -124,10 +124,10 @@ obj1 = enumerate(l1)
 obj2 = enumerate(s1)
   
 print ("Return type:", type(obj1))
-print ([enumerate(l1)])
+print (list(enumerate(l1)))
   
 # changing start index to 2 from 0
-print (list(enumerate(s1)))
+print (list(enumerate(s1,2)))
 
 
 
