94babfc4ff94defd665b19b6673c8a72bcf0a1b6
Abhilashg11 <alexrog1122@gmail.com>
2023-08-01 09:11:06 +0530

    main

--- Recursion/Factorial.py
+++ Recursion/Factorial.py
@@ -5,4 +5,10 @@ def fact(x):
    
 for i in range(6):
     print(fact(i),end = ' ')
+# a=[2,3,4,5]
+# for i in a:
+#     print(list(i+1 for i in a))
+
+
+
     
--- linked_list.py
+++ linked_list.py
@@ -116,19 +116,19 @@
 
 
 # def a(self,b: List[int]):
-l1 = ["eat", "sleep", "repeat"]
-s1 = "geek"
+# l1 = ["eat", "sleep", "repeat"]
+# s1 = "geek"
   
-# creating enumerate objects
-obj1 = enumerate(l1)
-obj2 = enumerate(s1)
+# # creating enumerate objects
+# obj1 = enumerate(l1)
+# obj2 = enumerate(s1)
   
-print ("Return type:", type(obj1))
-print (list(enumerate(l1)))
+# print ("Return type:", type(obj1))
+# print (list(enumerate(l1)))
   
-# changing start index to 2 from 0
-print (list(enumerate(s1,2)))
-print("cheking two sum")
+# # changing start index to 2 from 0
+# print (list(enumerate(s1,2)))
+# print("cheking two sum")
 
 
 
